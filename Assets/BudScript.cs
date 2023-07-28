21cc52bd6e64f67b7a88c1922acd76cf462c176e
btramel <66852498+btramel@users.noreply.github.com>
2023-07-28 11:51:07 -0500

    adds game over screen and logic

--- Assets/BudScript.cs
+++ Assets/BudScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class BudScript : MonoBehaviour
@@ -9,18 +10,25 @@ public class BudScript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     // creates variable in Unity to toggle instead of moving back and forth between code and Unity
     public float flapStrength;
+    public GameLogic logic;
+    public bool budIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<GameLogic>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true) {
+        if (Input.GetKeyDown(KeyCode.Space) == true && budIsAlive) {
         myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        budIsAlive = false;
+        logic.gameOver();
     }
 }
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour
 {
    public int playerScore;
    public Text scoreText;
+   public GameObject gameOverScreen;
 
     [ContextMenu("IncreaseScore")]
    public void addScore(int scoreToAdd) {
-    playerScore = playerScore + scoreToAdd;
+    playerScore += scoreToAdd;
     scoreText.text = playerScore.ToString();
+   }
+
+   public void restartGame() {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+
+   public void gameOver() {
+      gameOverScreen.SetActive(true);
    }
 }
